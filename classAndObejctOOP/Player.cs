ac3e1b1940229d79146ad991d4dfaa3ee7e8612a
Samiulahmedanik <84468942+SAANIK45@users.noreply.github.com>
2022-04-15 18:09:04 +0600

    Add project files.

---
+++ classAndObejctOOP/Player.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace classAndObejctOOP
+{
+    public class Player
+    {
+        public string name = "Anik";
+        public int health = 78;
+        public bool isBrazillian = true; 
+    }
+}
---
+++ classAndObejctOOP/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace classAndObejctOOP
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("OOP _ Class and Object");
+
+            Player zidan = new Player();
+            zidan.name = "zidan";
+            zidan.health = 34;
+            zidan.isBrazillian = false;
+
+            Player messi = new Player();
+            messi.name = "Messi";
+            messi.health = 100;
+            messi.isBrazillian = false;
+
+
+            Console.WriteLine(zidan.name);
+            Console.WriteLine(zidan.health);
+        }
+    }
+}
